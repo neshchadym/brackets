c90f743eb09d1fdf717d2d65753844f1ca76f26f
Vova <70287671+neshchadym@users.noreply.github.com>
2022-01-31 14:49:01 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let openCloseMap = {};
+    let closeOpenMap = {};
+    bracketsConfig.forEach (config => {
+            openCloseMap[config[0]] = config[1];
+            closeOpenMap[config[1]] = config[0];
+        }
+    );
+
+    let stack = [];
+    try {
+        str.split('').forEach(currChar => {
+            if (openCloseMap.hasOwnProperty(currChar)) { // if this is an open symbol
+                if (closeOpenMap.hasOwnProperty(currChar) && stack[stack.length - 1] === currChar) {
+                    stack.pop();
+                } else {
+                    stack.push(currChar);
+                }
+            } else if (closeOpenMap.hasOwnProperty(currChar)) { // if this is a close symbol
+                if (stack[stack.length - 1] === closeOpenMap[currChar]) {
+                    stack.pop();
+                } else {
+                    throw Error('---');
+                }
+            }
+        });
+    } catch (e) {
+        return false;
+    }
+
+    return stack.length === 0;
+};
